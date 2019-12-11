6b2887d3e86cb7bd8b7c9cc5e92ffee070f32c23
Bryan Swift <mail@bryanwrit.es>
2019-12-11 17:40:13 -0500

    Add simple validation to `Article` record

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# Article model to validate and perform CRUD operations on article data
 class Article < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
