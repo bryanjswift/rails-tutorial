4185db09f9f13f2e602485c1c6706834149ca8af
Bryan Swift <mail@bryanwrit.es>
2019-12-11 18:44:51 -0500

    Add `#edit` and `#update` actions to `ArticlesController`
    Including links to the editing from article list and article display.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,12 +15,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
