5759e8d209a7337a66c6490662b87a4448322293
Bryan Swift <mail@bryanwrit.es>
2019-12-11 20:16:40 -0500

    Generate `Comment` model and execute db migration

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@
 
 # Article model to validate and perform CRUD operations on article data
 class Article < ApplicationRecord
+  has_many :comments
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
